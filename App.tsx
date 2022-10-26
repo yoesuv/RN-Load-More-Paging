03655963c98c6197235ca4d5d361daf018b95e26
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-10-26 14:19:28 +0700

    :art: toolbar options

--- App.tsx
+++ App.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator, NativeStackNavigationOptions } from '@react-navigation/native-stack';
 import { RootStackParamsList } from './src/screens/root-stack-params';
 import Home from './src/screens/home';
+import { THEME_COLOR } from './src/data/colors';
 
 const Stack = createNativeStackNavigator<RootStackParamsList>();
+
+const baseOptions: NativeStackNavigationOptions = {
+  title: "RN Load More",
+  headerStyle: {
+    backgroundColor: THEME_COLOR
+  },
+  headerTintColor: 'white',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+  statusBarColor: THEME_COLOR,
+};
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Home" component={Home} options={{...baseOptions, title: "Home"}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
