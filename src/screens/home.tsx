f0a2d00a035701e0cc8b8f267fcb89d7c7df9ff3
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-10-26 13:47:16 +0700

    :sparkles: home ui

---
+++ src/screens/home.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet, SafeAreaView, Text, View, ScrollView } from 'react-native';
+import AppButton from '../components/button';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/native';
+import { RootStackParamsList } from './root-stack-params';
+import SizedBox from '../components/sized-box';
+
+type homeScreenProp = StackNavigationProp<RootStackParamsList, 'Home'>;
+
+export default function Home() {
+    const navigation = useNavigation<homeScreenProp>();
+    return <SafeAreaView style={styles.container}>
+        <View style={styles.content}>
+            <View style={styles.button} >
+                <AppButton title="Pagination List" onPress={() => {
+
+                }} />
+            </View>
+            <SizedBox height={14} />
+            <View style={styles.button} >
+                <AppButton title="Pagination Grid" onPress={() => {
+
+                }} />
+            </View>
+        </View>
+    </SafeAreaView>
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    content: {
+        paddingHorizontal: 24,
+        width: "100%",
+    },
+    button: {
+        height: 50,
+    }
+  });
