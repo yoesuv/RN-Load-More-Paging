8e98976a0f41345b73fc473fde4be94611ecd7ba
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-10-27 11:35:50 +0700

    :pencil: dummy list

--- src/screens/paging-list.tsx
+++ src/screens/paging-list.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { FlashList } from "@shopify/flash-list";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { StyleSheet, SafeAreaView, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, Text, View, Dimensions } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamsList } from './root-stack-params';
+import UsePosts from '../services/list-post-service';
 
 type listScreenProp = StackNavigationProp<RootStackParamsList, 'PagingList'>;
 const queryClient = new QueryClient();
@@ -10,12 +12,27 @@ const queryClient = new QueryClient();
 export default function PagingList() {
     return <QueryClientProvider client={queryClient}>
     <SafeAreaView>
-        <Text style={styles.label}>Paging List</Text>
+        <View style={styles.containerPaging}>
+            <FlashList 
+                data={[{name: 'tom'}, {name: 'jerry'}]} 
+                renderItem={({item}) => <ItemPost />}
+                estimatedItemSize={100}
+                />
+        </View>
     </SafeAreaView>
     </QueryClientProvider>
 }
 
+function ItemPost(): JSX.Element {
+    return <Text style={styles.label}>Paging List</Text>;
+}
+
 const styles = StyleSheet.create({
+    containerPaging: {
+        width: Dimensions.get('screen').width,
+        height: 300,
+        backgroundColor: 'yellow',
+    },
     label: {
         fontWeight: 'bold',
         fontSize: 24,
