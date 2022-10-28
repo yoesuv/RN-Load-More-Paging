f120e316466abd893b4d62954f6cf2e21642f379
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-10-28 12:51:47 +0700

    :pencil: add loading

--- src/screens/paging-grid.tsx
+++ src/screens/paging-grid.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, Text, View, ActivityIndicator } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamsList } from './root-stack-params';
 
@@ -11,7 +11,18 @@ export default function PagingGrid() {
     </SafeAreaView>
 }
 
+function Loading(): JSX.Element {
+    return <View style={styles.containerCenter}>
+        <ActivityIndicator size={'large'} />
+    </View>
+}
+
 const styles = StyleSheet.create({
+    containerCenter: {
+        flex: 1,
+        justifyContent: 'center',
+        alignContent: 'center',
+    },
     label: {
         fontWeight: 'bold',
         fontSize: 24,
