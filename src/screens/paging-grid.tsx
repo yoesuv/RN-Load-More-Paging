0dbfb8902474107548da3ef68913eb228feffa22
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-10-28 13:14:58 +0700

    :construction: show grid list

--- src/screens/paging-grid.tsx
+++ src/screens/paging-grid.tsx
@@ -1,14 +1,49 @@
 import React from 'react';
+import { FlashList } from "@shopify/flash-list";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StyleSheet, SafeAreaView, Text, View, ActivityIndicator } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamsList } from './root-stack-params';
+import UsePosts from '../services/list-post-service';
+import ItemPostList from './item-post-list';
 
 type gridScreenProp = StackNavigationProp<RootStackParamsList, 'PagingGrid'>;
 
+const queryClient = new QueryClient();
+
 export default function PagingGrid() {
-    return <SafeAreaView>
-        <Text style={styles.label}>Paging Grid</Text>
-    </SafeAreaView>
+    return <QueryClientProvider client={queryClient}> 
+        <SafeAreaView style={styles.container}>
+            <Content />
+        </SafeAreaView>
+    </QueryClientProvider>
+}
+
+function Content(): JSX.Element {
+    const {
+        data,
+        error,
+        fetchNextPage,
+        hasNextPage,
+        isFetching,
+        isFetchingNextPage,
+        status,
+    } = UsePosts();
+    const flattenData = data?.pages.flatMap(page => page.data);
+
+    if (status === 'loading') {
+        return <Loading />
+    }
+
+    return <View style={styles.containerPaging}>
+        <FlashList 
+            numColumns={2}
+            data={flattenData}
+            renderItem={({item}) => <ItemPostList post={item} /> }
+            estimatedItemSize={100}
+            onEndReachedThreshold={0.3}
+        />
+    </View>
 }
 
 function Loading(): JSX.Element {
@@ -18,6 +53,10 @@ function Loading(): JSX.Element {
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
     containerCenter: {
         flex: 1,
         justifyContent: 'center',
@@ -26,5 +65,8 @@ const styles = StyleSheet.create({
     label: {
         fontWeight: 'bold',
         fontSize: 24,
-    }
+    },
+    containerPaging: {
+        flex: 1,
+    },
 })
